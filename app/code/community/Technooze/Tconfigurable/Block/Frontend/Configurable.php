bc2af9347a81f46c18aa78d64c3d625a03461205
damu <damodar@acidgreen.com.au>
2012-11-26 11:08:30 +1100

    first commit

---
+++ app/code/community/Technooze/Tconfigurable/Block/Frontend/Configurable.php
@@ -0,0 +1,109 @@
+<?php
+
+class Technooze_Tconfigurable_Block_Frontend_Configurable extends Mage_Catalog_Block_Product_View_Type_Configurable
+{
+	public function getRegularConfig($attribute_id=0)
+    {
+        $attributes = array();
+        $options = array();
+        $store = Mage::app()->getStore();
+        foreach ($this->getAllowProducts() as $product) {
+            $productId  = $product->getId();
+
+            foreach ($this->getAllowAttributes() as $attribute) {
+                $productAttribute = $attribute->getProductAttribute();
+                $attributeValue = $product->getData($productAttribute->getAttributeCode());
+                if (!isset($options[$productAttribute->getId()])) {
+                    $options[$productAttribute->getId()] = array();
+                }
+
+                if (!isset($options[$productAttribute->getId()][$attributeValue])) {
+                    $options[$productAttribute->getId()][$attributeValue] = array();
+                }
+                $options[$productAttribute->getId()][$attributeValue][] = $productId;
+            }
+        }
+
+        $this->_resPrices = array(
+            $this->_preparePrice($this->getProduct()->getFinalPrice())
+        );
+
+        foreach ($this->getAllowAttributes() as $attribute) {
+            $productAttribute = $attribute->getProductAttribute();
+            $attributeId = $productAttribute->getId();
+            $info = array(
+               'id'        => $productAttribute->getId(),
+               'code'      => $productAttribute->getAttributeCode(),
+               'label'     => $attribute->getLabel(),
+               'options'   => array()
+            );
+
+            $optionPrices = array();
+            $prices = $attribute->getPrices();
+            if (is_array($prices)) {
+                foreach ($prices as $value) {
+                    if(!$this->_validateAttributeValue($attributeId, $value, $options)) {
+                        continue;
+                    }
+
+                    $info['options'][] = array(
+                        'id'    => $value['value_index'],
+                        'label' => $value['label'],
+                        'price' => $this->_preparePrice($value['pricing_value'], $value['is_percent']),
+                        'products'   => isset($options[$attributeId][$value['value_index']]) ? $options[$attributeId][$value['value_index']] : array(),
+                    );
+                    $optionPrices[] = $this->_preparePrice($value['pricing_value'], $value['is_percent']);
+                    //$this->_registerAdditionalJsPrice($value['pricing_value'], $value['is_percent']);
+                }
+            }
+            /**
+             * Prepare formated values for options choose
+             */
+            foreach ($optionPrices as $optionPrice) {
+                foreach ($optionPrices as $additional) {
+                    $this->_preparePrice(abs($additional-$optionPrice));
+                }
+            }
+            if($this->_validateAttributeInfo($info)) {
+               $attributes[$attributeId] = $info;
+            }
+        }
+        /*echo '<pre>';
+        print_r($this->_prices);
+        echo '</pre>';die();*/
+
+        $_request = Mage::getSingleton('tax/calculation')->getRateRequest(false, false, false);
+        $_request->setProductClassId($this->getProduct()->getTaxClassId());
+        $defaultTax = Mage::getSingleton('tax/calculation')->getRate($_request);
+
+        $_request = Mage::getSingleton('tax/calculation')->getRateRequest();
+        $_request->setProductClassId($this->getProduct()->getTaxClassId());
+        $currentTax = Mage::getSingleton('tax/calculation')->getRate($_request);
+
+        $taxConfig = array(
+            'includeTax'        => Mage::helper('tax')->priceIncludesTax(),
+            'showIncludeTax'    => Mage::helper('tax')->displayPriceIncludingTax(),
+            'showBothPrices'    => Mage::helper('tax')->displayBothPrices(),
+            'defaultTax'        => $defaultTax,
+            'currentTax'        => $currentTax,
+            'inclTaxTitle'      => Mage::helper('catalog')->__('Incl. Tax'),
+        );
+
+        $config = array(
+            'attributes'        => $attributes,
+            'template'          => str_replace('%s', '#{price}', $store->getCurrentCurrency()->getOutputFormat()),
+//            'prices'          => $this->_prices,
+            'basePrice'         => $this->_registerJsPrice($this->_convertPrice($this->getProduct()->getFinalPrice())),
+            'oldPrice'          => $this->_registerJsPrice($this->_convertPrice($this->getProduct()->getPrice())),
+            'productId'         => $this->getProduct()->getId(),
+            'chooseText'        => Mage::helper('catalog')->__('Choose an Option...'),
+            'taxConfig'         => $taxConfig,
+        );
+
+        if($attribute_id && isset($config['attributes'][$attribute_id])){
+            return $config['attributes'][$attribute_id];
+        }
+
+        return $config;
+    }
+}
